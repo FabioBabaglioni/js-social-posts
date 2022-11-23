93c399a8680018acb18cd234d35c700b9fa52b5e
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-23 16:09:06 +0100

    correzzione stap post

--- main.js
+++ main.js
@@ -69,36 +69,39 @@ const posts = [
 
 posts.forEach(utente => {
 
-    let post = document.querySelector(".post")
+    let post = document.getElementById("container")
 
-    post.innerHTML += `<div class="post__header">
-    <div class="post-meta">                    
-        <div class="post-meta__icon">
-            <img class="profile-pic" src="${utente.author.image}" alt="">                    
+    post.innerHTML += `
+    <div class="post">
+        <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${utente.author.image}" alt="">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${utente.author.name}</div>
+                <div class="post-meta__time">${utente.created}</div>
+            </div>                    
         </div>
-        <div class="post-meta__data">
-            <div class="post-meta__author">${utente.author.name}</div>
-            <div class="post-meta__time">${utente.created}</div>
-        </div>                    
-    </div>
-    </div>
-    <div class="post__text">${utente.content}</div>
-    <div class="post__image">
-        <img src="${utente.media}" alt="">
-    </div>
-    <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-                    </div>
-                </div> 
-            </div>  `
+        </div>
+        <div class="post__text">${utente.content}</div>
+        <div class="post__image">
+            <img src="${utente.media}" alt="">
+        </div>
+        <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        </div>
+                    </div> 
+                </div>  
+    </div>`
     
 
 });
